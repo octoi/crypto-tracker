f48aa5d1e36ea27956cf5c43f10c262a3acd3673
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-12 14:40:45 +0530

    used coins

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import SearchBar from '../components/SearchBar'
+import Coins from '../components/Coins'
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <SearchBar type="text" placeholder="Search" />
+            <Coins />
         </div>
     )
 }

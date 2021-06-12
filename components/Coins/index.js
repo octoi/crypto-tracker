e29473699fe00e208ab9851d3a313a7270a935e2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-12 15:02:34 +0530

    getting props

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Coins = ({ name }) => {
+const Coins = ({ id, name, price, symbol, marketcap, volume, image, priceChange }) => {
 	return (
 		<div>
 			<h1>{name}</h1>
+
 		</div>
 	)
 }

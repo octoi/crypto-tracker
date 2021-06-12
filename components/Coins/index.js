6b52ed303ab417b9142cfa98d36c2f50c45de56e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-12 15:08:47 +0530

    displayed all items

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -2,9 +2,26 @@ import React from 'react'
 
 const Coins = ({ id, name, price, symbol, marketcap, volume, image, priceChange }) => {
 	return (
-		<div>
-			<h1>{name}</h1>
-
+		<div className="coin_container">
+			<div className="coin_row">
+				<div className="coin">
+					<img src={image} alt={id} />
+					<h1 className="coin_h1">{name}</h1>
+					<p className="coin_symbol">{symbol}</p>
+				</div>
+				<div className="coin_data">
+					<p className="coin_price">₹{price}</p>
+					<p className="coin_volume">₹{volume.toLocaleString()}</p>
+					{priceChange < 0 ? (
+						<p className="coin_percent red">{priceChange.toFixed(2)}%</p>
+					) : (
+						<p className="coin_percent green">{priceChange.toFixed(2)}%</p>
+					)}
+					<p className="coin_market_cap">
+						Mkt Cap: ₹{marketcap.toLocaleString()}
+					</p>
+				</div>
+			</div>
 		</div>
 	)
 }
